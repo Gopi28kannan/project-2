3f6b091d717d1aabe01e43d8f994367cdfcb8afe
Gopi28kannan <144014765+Gopi28kannan@users.noreply.github.com>
2024-01-01 17:29:47 +0530

    Update main.py

--- PROJECT_2/main.py
+++ PROJECT_2/main.py
@@ -9,23 +9,24 @@ import Validate_header as VH
 #Test case id 3 run as Validate_menu_list
 import Validate_menu_list  as VML
         
+#forget password
 def test_1():
      FP.webpage1().get_url()
      FP.webpage1().forget_password()
 
-         
+#validate title         
 def test_2():
      VH.webpage2().get_url()
      VH.webpage2().login()
      VH.webpage2().validate_admin_page_title()
      print("validate admin page title successfully runned\n")
 
-
+#validate admin page options
 def test_3():
      VH.webpage2().validate_admin_page_options()
      print("\nvalidate admin page options successfully runned\n")
 
-
+#validate menu list options
 def test_4():
      VML.webpage3().get_url()
      VML.webpage3().login()
